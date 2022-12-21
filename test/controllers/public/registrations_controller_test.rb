fe8dff2bc2a65cd766a5db9a364f137cee5560d1
EC2 Default User <ec2-user@ip-172-31-27-33.ap-northeast-1.compute.internal>
2022-12-21 22:16:06 +0000

    repair_migrate

--- test/controllers/public/registrations_controller_test.rb
+++ test/controllers/public/registrations_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PublicTest < ActiveSupport::TestCase
+class Public::RegistrationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
