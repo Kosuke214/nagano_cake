f1db06b80706b851e77deda45b83d8008c090cb5
EC2 Default User <ec2-user@ip-172-31-27-33.ap-northeast-1.compute.internal>
2022-12-24 23:25:26 +0000

    3.1.4-complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,29 @@
 Rails.application.routes.draw do
+
+  #root to: "public/homes#top"
+
   devise_for :customers, controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
-}
+  }
+
   devise_for :admin, controllers: {
   sessions: "admin/sessions"
-}
+  }
+
+  resources :items, only: [:index, :show]
+  resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+  resources :oders, only: [:new, :comfirm, :complete, :create, :index, :show]
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    patch'details' => 'dedails#update'
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
